354e3dccc8b2d8042489750611b16325d9e7767b
hemant <hemantarya7860@gmail.com>
2024-09-08 19:55:17 +0530

    feat: wrap all api in try-catch

--- index.js
+++ index.js
@@ -36,87 +36,135 @@ const CartSchema = new mongoose.Schema({
 //Product CRUD API
 
 app.get("/", async (req, res) => {
-  const allProductData = await product.find();
+  try {
+    const allProductData = await product.find();
 
-  res.json({ message: "request recive", data: allProductData });
+    res.json({ message: "request recive", data: allProductData });
+  } catch (error) {
+    console.log(error);
+  }
 });
 app.get("/product/:id", async (req, res) => {
-  const singleProductId = req.params.id;
+  try {
+    const singleProductId = req.params.id;
 
-  const oneProductData = await product.findById(singleProductId);
+    const oneProductData = await product.findById(singleProductId);
 
-  res.json({ message: "request recive", data: oneProductData });
+    res.json({ message: "request recive", data: oneProductData });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.post("/createproduct", async (req, res) => {
-  const productData = await req.body;
-  const newProduct = new product(productData);
-  newProduct.save();
+  try {
+    const productData = await req.body;
+    const newProduct = new product(productData);
+    newProduct.save();
 
-  res.json({ message: "product revice", data: productData });
+    res.json({ message: "product revice", data: productData });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.delete("/deleteproduct/:id", async (req, res) => {
-  const deleteProductId = req.params.id;
+  try {
+    const deleteProductId = req.params.id;
 
-  const deleteProduct = await product.findByIdAndDelete(deleteProductId);
+    const deleteProduct = await product.findByIdAndDelete(deleteProductId);
 
-  res.json({ message: "id recive", id: deleteProductId });
+    res.json({ message: "id recive", id: deleteProductId });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.put("/updateproduct/:id", async (req, res) => {
-  const editProductId = req.params.id;
-  const editProductItem = req.body;
+  try {
+    const editProductId = req.params.id;
+    const editProductItem = req.body;
 
-  const updateData = await product.findByIdAndUpdate(
-    editProductId,
-    editProductItem,
-    {
-      new: true,
-    }
-  );
+    const updateData = await product.findByIdAndUpdate(
+      editProductId,
+      editProductItem,
+      {
+        new: true,
+      }
+    );
 
-  res.json({ message: "id recive", data: updateData });
+    res.json({ message: "id recive", data: updateData });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // user CRUD API
+
+//user list
 app.get("/users", async (req, res) => {
-  const allusers = await productUser.find();
-  res.json({ message: "all user data", data: allusers });
+  try {
+    const allusers = await productUser.find();
+    res.json({ message: "all user data", data: allusers });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+//create new user
 app.post("/new_users", async (req, res) => {
-  const user = req.body;
-  const newUser = await new productUser(user);
-  newUser.save();
+  try {
+    const user = req.body;
+    const newUser = await new productUser(user);
+    newUser.save();
 
-  res.json({ message: "user save successfully" });
+    res.json({ message: "user save successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+//find user by id
 app.get("/users/:id", async (req, res) => {
-  const userId = req.params.id;
+  try {
+    const userId = req.params.id;
 
-  const findUser = await productUser.findById(userId);
-  res.json({ message: "id recive", data: findUser });
+    const findUser = await productUser.findById(userId);
+    res.json({ message: "id recive", data: findUser });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+//delete user by id
 
 app.delete("/delete_user/:id", async (req, res) => {
-  const deleteUserId = req.params.id;
-  const deleteUser = await productUser.findByIdAndDelete(deleteUserId);
+  try {
+    const deleteUserId = req.params.id;
+    const deleteUser = await productUser.findByIdAndDelete(deleteUserId);
 
-  res.json({ message: "user delete successfully" });
+    res.json({ message: "user delete successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.put("/update_user/:id", async (req, res) => {
-  const updateUserId = req.params.id;
-  const updateUserData = req.body;
-  const updateUser = await productUser.findByIdAndUpdate(
-    updateUserId,
-    updateUserData,
-    { new: true }
-  );
+//update user by id
 
-  res.json({ message: "user update successfully", data: updateUser });
+app.put("/update_user/:id", async (req, res) => {
+  try {
+    const updateUserId = req.params.id;
+    const updateUserData = req.body;
+    const updateUser = await productUser.findByIdAndUpdate(
+      updateUserId,
+      updateUserData,
+      { new: true }
+    );
+
+    res.json({ message: "user update successfully", data: updateUser });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //cart api
@@ -138,95 +186,125 @@ app.put("/update_user/:id", async (req, res) => {
 
 // find cart by id
 app.get("/cart/:id", async (req, res) => {
-  const reciveUserId = req.params.id;
-  const findUserById = await cart.find({ userId: reciveUserId });
+  try {
+    const reciveUserId = req.params.id;
+    const findUserById = await cart.find({ userId: reciveUserId });
 
-  res.json({ message: "recive user id", id: reciveUserId, data: findUserById });
+    res.json({
+      message: "recive user id",
+      id: reciveUserId,
+      data: findUserById,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //Add quantity
 
 app.put("/cart/increase/:id", async (req, res) => {
-  const userId = req.params.id;
-  const productId = req.body.productId;
+  try {
+    const userId = req.params.id;
+    const productId = req.body.productId;
 
-  const foundCart = await cart.findOne({ userId });
-  const foundIndex = foundCart.product.findIndex(
-    (item) => item._id == productId
-  );
-  console.log(foundIndex);
-  foundCart.product[foundIndex].quantity =
-    foundCart.product[foundIndex].quantity + 1;
-  const result = await foundCart.save();
+    const foundCart = await cart.findOne({ userId });
+    const foundIndex = foundCart.product.findIndex(
+      (item) => item._id == productId
+    );
 
-  res.json({ message: "recive", data: result });
+    foundCart.product[foundIndex].quantity =
+      foundCart.product[foundIndex].quantity + 1;
+    const result = await foundCart.save();
+
+    res.json({ message: "recive", data: result });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // decrease quantity
 
 app.put("/cart/decrease/:id", async (req, res) => {
-  const userId = req.params.id;
-  const productId = req.body.productId;
+  try {
+    const userId = req.params.id;
+    const productId = req.body.productId;
 
-  const foundCart = await cart.findOne({ userId });
-  const foundIndex = foundCart.product.findIndex(
-    (item) => item._id == productId
-  );
+    const foundCart = await cart.findOne({ userId });
+    const foundIndex = foundCart.product.findIndex(
+      (item) => item._id == productId
+    );
 
-  foundCart.product[foundIndex].quantity =
-    foundCart.product[foundIndex].quantity - 1;
+    foundCart.product[foundIndex].quantity =
+      foundCart.product[foundIndex].quantity - 1;
 
-  const result = await foundCart.save();
+    const result = await foundCart.save();
 
-  res.json({ message: "decrease quantity", data: result });
+    res.json({ message: "decrease quantity", data: result });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // remove item
 app.delete("/cart/removeitem/:id", async (req, res) => {
-  const userId = req.params.id;
-  const productId = req.body.productId;
+  try {
+    const userId = req.params.id;
+    const productId = req.body.productId;
 
-  const foundCart = await cart.findOne({ userId });
+    const foundCart = await cart.findOne({ userId });
 
-  foundCart.product = foundCart.product.filter((item) => item._id != productId);
+    foundCart.product = foundCart.product.filter(
+      (item) => item._id != productId
+    );
 
-  foundCart.save();
+    foundCart.save();
 
-  res.json({
-    message: "item remove",
-  });
+    res.json({
+      message: "item remove",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //add item in cart
 
 app.post("/cart/addItem/:id", async (req, res) => {
-  const userId = req.params.id;
-  const productId = req.body.productId;
+  try {
+    const userId = req.params.id;
+    const productId = req.body.productId;
 
-  const foundCart = await cart.findOne({ userId });
-  const findProduct = await product.findById(productId);
+    const foundCart = await cart.findOne({ userId });
+    const findProduct = await product.findById(productId);
 
-  foundCart.product.push(findProduct);
+    foundCart.product.push(findProduct);
 
-  foundCart.save();
+    foundCart.save();
 
-  res.json({ message: "add value in cart ", data: findProduct });
+    res.json({ message: "add value in cart ", data: findProduct });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // ? how to do total random idea
 
 app.get("/cart/totalprice/:id", async (req, res) => {
-  const userId = req.params.id;
-  const foundCart = await cart.findOne({ userId });
-  const priceArray = [];
+  try {
+    const userId = req.params.id;
+    const foundCart = await cart.findOne({ userId });
+    const priceArray = [];
 
-  foundCart.product.map((item) => priceArray.push(item.price));
+    foundCart.product.map((item) => priceArray.push(item.price));
 
-  const total = priceArray.reduce(
-    (accumulator, currentValue) => accumulator + currentValue
-  );
+    const total = priceArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
 
-  res.json({ message: "calculation of total price", total: total });
+    res.json({ message: "calculation of total price", total: total });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //model
